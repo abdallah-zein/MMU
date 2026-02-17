bb649dbe28728bbc3a661e0ae6d77ddd453bbd64
abdallah-zein <abdallahzein.eng@gmail.com>
2026-02-17 20:43:10 +0200

    Update bias and out data types in PE_block module

--- src/PE_block.sv
+++ src/PE_block.sv
@@ -1,16 +1,16 @@
 module PE_block (in, w, bias, out);
     input   [7:0] in [0:6][0:3];
     input   [7:0] w [0:3];
-    input   [15:0] bias [0:6];
-    output  [15:0] out [0:6]; // look into overflow
+    input   [31:0] bias;
+    output  [31:0] out [0:6]; // look into overflow
     
-    wire [15:0] mac_out [0:6][0:3];
+    wire [31:0] mac_out [0:6][0:3];
     genvar i,j;
     generate
         for (i=0; i<7 ; i++) begin
             for (j=0; j<4 ; j++) begin
                 if (j==0) begin
-                    mac m (.in_a(in[i][j]), .in_b(w[j]), .in_c(bias[i]), .out(mac_out[i][j]));
+                    mac m (.in_a(in[i][j]), .in_b(w[j]), .in_c(bias), .out(mac_out[i][j]));
                 end
                 else begin
                     mac m (.in_a(in[i][j]), .in_b(w[j]), .in_c(mac_out[i][j-1]), .out(mac_out[i][j]));
