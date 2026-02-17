f95ea7a57c4f5995cc8861cf477c89e852951a0f
abdallah-zein <abdallahzein.eng@gmail.com>
2026-02-17 20:45:14 +0200

    Increase bit width of inputs and outputs in mac module

--- src/mac.sv
+++ src/mac.sv
@@ -1,11 +1,11 @@
 module mac(in_a, in_b, in_c, out);
     input  [7:0] in_a;
     input  [7:0] in_b;
-    input  [15:0] in_c;
-    output [15:0] out;
+    input  [31:0] in_c;
+    output [31:0] out;
 
-    wire [15:0] mul_res;
-    assign  mul_res = {{8{in_a[7]}},in_a} * {{8{in_b[7]}},in_b};
+    wire [31:0] mul_res;
+    assign  mul_res = {{24{in_a[7]}},in_a} * {{24{in_b[7]}},in_b};
     assign  out = mul_res + in_c;
 
 endmodule
